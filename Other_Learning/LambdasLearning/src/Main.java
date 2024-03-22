68e5a0b8fcf23a300196857c692e5a87e1609127
Naspo <a.topaltsis@gmail.com>
2024-03-22 16:11:15 -0400

    learned java lambdas

--- Other_Learning/LambdasLearning/src/Main.java
+++ Other_Learning/LambdasLearning/src/Main.java
@@ -1,5 +1,21 @@
+// Lambdas Learning
+
+import java.util.ArrayList;
+
+// Lambdas are methods that can be defined and used as a variable/object.
+// Similar to arrow functions in javascript.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Example 1
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+
+        numbers.forEach((n) -> System.out.println(n));
+
+        // Example 2
+        StringFunction exclaim = (s) -> System.out.println(s + "!");
+        exclaim.run("Hello World");
     }
 }
---
+++ Other_Learning/LambdasLearning/src/StringFunction.java
@@ -0,0 +1,3 @@
+public interface StringFunction {
+    void run(String str);
+}
