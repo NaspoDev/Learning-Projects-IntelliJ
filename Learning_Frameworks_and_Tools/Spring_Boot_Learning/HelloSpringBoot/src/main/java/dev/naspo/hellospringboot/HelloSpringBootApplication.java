bdad85ff4d390785f19e4f620d3534bdf1d85c8a
Athanasios Topaltsis <a.topaltsis@gmail.com>
2024-04-16 15:51:24 -0400

    Added comments to HelloSpringBoot

--- Learning_Frameworks_and_Tools/Spring_Boot_Learning/HelloSpringBoot/src/main/java/dev/naspo/hellospringboot/HelloSpringBootApplication.java
+++ Learning_Frameworks_and_Tools/Spring_Boot_Learning/HelloSpringBoot/src/main/java/dev/naspo/hellospringboot/HelloSpringBootApplication.java
@@ -8,14 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 // Following the Spring Boot quick-start guide: https://spring.io/quickstart
 
-@SpringBootApplication
-@RestController
+@SpringBootApplication // This annotation is used to define the class as a Spring Boot application.
+@RestController // This annotation is used to define the class as a REST controller.
 public class HelloSpringBootApplication {
 
+	// The main method is the entry point of the application.
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringBootApplication.class, args);
 	}
 
+	// This method is used to define a route that returns a greeting message.
+	// @RequestParam is used to define a query parameter.
 	@GetMapping("/hello")
 	public String helloRoute(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
