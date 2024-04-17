615b946c4e9a0ba2132846aea8935784f8bcd1cd
Athanasios Topaltsis <a.topaltsis@gmail.com>
2024-04-17 15:04:36 -0400

    Continuing learning Spring Boot
    Learning post requests

--- Learning_Frameworks_and_Tools/Spring_Boot_Learning/PhotosClone/src/main/java/dev/naspo/photosclone/Photo.java
+++ Learning_Frameworks_and_Tools/Spring_Boot_Learning/PhotosClone/src/main/java/dev/naspo/photosclone/Photo.java
@@ -1,12 +1,28 @@
 package dev.naspo.photosclone;
 
+import java.util.UUID;
+
 // Photo Data Class
 public class Photo {
     private String id;
     private String fileName;
 
-    public Photo(String id, String fileName) {
-        this.id = id;
+     /*
+     Data classes that need to be deserialized from JSON need to also have a 0 arg constructor.
+     It has to do with how the JSON deserialization library works.
+
+     * Note that the deserializer runs through the no-arg constructor and creates the object
+     based on the fields defined in the class, not based on the args defied in the regular constructor.
+     So, if you want to run logic alongside the creation of a class when it's being deserialized,
+     you must also add the logic to the no-arg constructor.
+        - Notice below how creating the id is in both constructors.
+      */
+    public Photo() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public Photo(String fileName) {
+        this.id = UUID.randomUUID().toString();
         this.fileName = fileName;
     }
 
--- Learning_Frameworks_and_Tools/Spring_Boot_Learning/PhotosClone/src/main/java/dev/naspo/photosclone/PhotosController.java
+++ Learning_Frameworks_and_Tools/Spring_Boot_Learning/PhotosClone/src/main/java/dev/naspo/photosclone/PhotosController.java
@@ -1,19 +1,22 @@
 package dev.naspo.photosclone;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 // The @RestController annotation defines a class as a REST controller. (The class handles requests).
 @RestController
-public class MyController {
-
+public class PhotosController {
     // mock database of photos.
-    private List<Photo> db = List.of(new Photo("1", "hello.jpg"));
+    private List<Photo> db = new ArrayList<>();
+
+    public PhotosController() {
+        db.add(new Photo("hello.jpg"));
+    }
 
     // @GetMapping("/route") defines this method as a GET method for the specified route.
     @GetMapping("/")
@@ -38,5 +41,27 @@ public class MyController {
 
         // Throw 404 error if photos doesn't exist.
         throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    // Delete a photo.
+    @DeleteMapping("/photos/{id}")
+    public void deletePhoto(@PathVariable String id) {
+        for (int i = 0; i < db.size(); i++) {
+            if (db.get(i).getId().equals(id)) {
+                db.remove(i);
+                return;
+            }
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    // Post a photo.
+    // @RequestBody annotation automatically converts the JSON post request body into
+    // the Java object provided. (In this case it will create a Photo object with it).
+    @PostMapping("/photos")
+    public Photo createPhoto(@RequestBody Photo photo) {
+        db.add(photo);
+        return photo;
     }
 }
