b017d79b93ae8920b3dd4ef2661a9679effdeb70
Athanasios Topaltsis <a.topaltsis@gmail.com>
2024-04-17 23:54:13 -0400

    removed unused dependency

--- Learning_Frameworks_and_Tools/Spring_Boot_Learning/PhotosClone/src/main/java/dev/naspo/photosclone/PhotosController.java
+++ Learning_Frameworks_and_Tools/Spring_Boot_Learning/PhotosClone/src/main/java/dev/naspo/photosclone/PhotosController.java
@@ -1,7 +1,6 @@
 package dev.naspo.photosclone;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
