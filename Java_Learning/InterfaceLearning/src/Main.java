3795f5931c2e8cef276ad6bb5a4a0eb88a5bceee
Athanasios Topaltsis <a.topaltsis@gmail.com>
2024-04-29 12:48:08 -0400

    updated comments in interface learning

--- Java_Learning/InterfaceLearning/src/Main.java
+++ Java_Learning/InterfaceLearning/src/Main.java
@@ -6,15 +6,17 @@ public class Main {
 
         // In this project example, we will show the difference between inheritance (extends) and an interface.
 
-        // Notable things about interfaces:
-        // - The implementing class has to have every method from the interface.
-        // - A class can implement multiple interfaces, allowing for multiple inheritance of behavior.
-        // - Interfaces are ideal for defining common behavior that multiple classes can share,
-        // irrespective of their inheritance hierarchy.
-        // - All methods in an interface are abstract.
-        // - All methods in an interface are automatically public.  The keyword public does not appear.
-        // - Interfaces do not have instance variables because no objects of the interface type can be created!
-        // - Any variables declared in an interface are automatically public, static, and final (i.e. constants).
+        /*
+         Notable things about interfaces:
+         - The implementing class has to have every method from the interface.
+         - A class can implement multiple interfaces, allowing for multiple inheritance of behavior.
+         - Interfaces are ideal for defining common behavior that multiple classes can share,
+           irrespective of their inheritance hierarchy.
+         - All methods in an interface are abstract.
+         - All methods in an interface are automatically public.  The keyword public does not appear.
+         - Interfaces do not have instance variables because no objects of the interface type can be created!
+         - Any variables declared in an interface are automatically public, static, and final (i.e. constants).
+          */
 
         // This example:
         // Say we want to make a water bottle, we inherit (extend) from the main WaterBottle class.
